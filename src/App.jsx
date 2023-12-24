c3870904055391d6f6c45347f91da3932ba811c9
nathantye <tyenathan@yahoo.co.uk>
2023-12-24 10:06:59 +0000

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ export default function App() {
   console.log;
   const [todos, setTodos] = useState([]);
 
+
   useEffect(() => {
     fetchToDos();
   }, []);
@@ -19,74 +20,77 @@ export default function App() {
     console.log(await todos.json());
   };
 
+  
+
   /* platform Launch fs-l  */
   return (
     <>
       <div className="container">
-        <div id="platform-launch">
-          <img src={mobileLogo} alt="" className="logo" />
-          <h2 className="fs-l">Platform Launch</h2>
-          <img src={chevronDown} alt="" />
-          <button>
+        <div id="topbar">
+          <img src={mobileLogo} alt="" id="mobile-logo" />
+          <h2 className="fs-l" id="board-name" >Platform Launch</h2>
+          <img src={chevronDown} alt="" id="chevron-down"/>
+          <button id="add-task-btn">
             <img src={addTaskMobile} alt="" />
           </button>
-          <img src={verticalEllipsis} alt="" />
+          <img src={verticalEllipsis} alt=""id="vertical-ellipsis"/>
         </div>
-      </div>
-      <div id="board">
-        <div className="board-column">
-          <h4 className="fs-s">
-            <ul className="board-list">
-              <li className="board-list-item">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Officiis, exercitationem.
-              </li>
-              <li className="board-list-item">
-                Itaque, adipisci reprehenderit a ratione labore voluptas quasi
-                nam debitis!
-              </li>
-              <li className="board-list-item">
-                Saepe nemo omnis nisi sed blanditiis eligendi incidunt ullam
-                dolore!
-              </li>
-            </ul>
-          </h4>
-        </div>
-        <div className="board-column">
-          <h4 className="fs-s">
-            <ul className="board-list">
-              <li className="board-list-item">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Explicabo, minus?
-              </li>
-              <li className="board-list-item">
-                Delectus sequi excepturi maxime. Temporibus magnam quae aliquam
-                harum quam.
-              </li>
-              <li className="board-list-item">
-                Nostrum at maiores earum adipisci reprehenderit autem architecto
-                necessitatibus commodi!
-              </li>
-            </ul>
-          </h4>
-        </div>
-        <div className="board-column">
-          <h4 className="fs-s">
-            <ul className="board-list">
-              <li className="board-list-item">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Architecto, necessitatibus.
-              </li>
-              <li className="board-list-item">
-                Qui magni, harum debitis totam officiis nesciunt placeat? Ipsam,
-                mollitia.
-              </li>
-              <li className="board-list-item">
-                Quas voluptatum tenetur quo quia architecto eaque quam
-                blanditiis expedita!
-              </li>
-            </ul>
-          </h4>
+
+        <div id="board">
+          <div className="board-column">
+            <h4 className="fs-s">
+              <ul className="board-list">
+                <li className="board-list-item">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Officiis, exercitationem.
+                </li>
+                <li className="board-list-item">
+                  Itaque, adipisci reprehenderit a ratione labore voluptas quasi
+                  nam debitis!
+                </li>
+                <li className="board-list-item">
+                  Saepe nemo omnis nisi sed blanditiis eligendi incidunt ullam
+                  dolore!
+                </li>
+              </ul>
+            </h4>
+          </div>
+          <div className="board-column">
+            <h4 className="fs-s">
+              <ul className="board-list">
+                <li className="board-list-item">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Explicabo, minus?
+                </li>
+                <li className="board-list-item">
+                  Delectus sequi excepturi maxime. Temporibus magnam quae
+                  aliquam harum quam.
+                </li>
+                <li className="board-list-item">
+                  Nostrum at maiores earum adipisci reprehenderit autem
+                  architecto necessitatibus commodi!
+                </li>
+              </ul>
+            </h4>
+          </div>
+          <div className="board-column">
+            <h4 className="fs-s">
+              <ul className="board-list">
+                <li className="board-list-item">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Architecto, necessitatibus.
+                </li>
+                <li className="board-list-item">
+                  Qui magni, harum debitis totam officiis nesciunt placeat?
+                  Ipsam, mollitia.
+                </li>
+                <li className="board-list-item">
+                  Quas voluptatum tenetur quo quia architecto eaque quam
+                  blanditiis expedita!
+                </li>
+              </ul>
+            </h4>
+          </div>
         </div>
       </div>
     </>
